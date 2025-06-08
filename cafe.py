6ec272f2646ff7c0785aebd084f0b985db437e69
Maximus Brutalis <dmitroid1sk@gmail.com>
2025-06-08 09:53:35 +0700

    Add user input handling for drink selection

--- cafe.py
+++ cafe.py
@@ -2,4 +2,27 @@ from coffee_machine import CoffeeMachine
 
 coffee_machine = CoffeeMachine()
 
-coffee_machine.brew("Cold Espresso")
+def user_input():
+    return input("Choose your drink:\n1. Hot Espresso\n2. Cold Espresso\n3. Hot Latte\n4. Latte Affogato\n")
+
+while True:
+    drink = user_input()
+    
+    if drink =="1":
+        drink = "Hot Espresso"
+        break
+    elif drink == "2":
+        drink = "Cold Espresso"
+        break
+    elif drink == "3":
+        drink = "Hot Latte"
+        break
+    elif drink == "4":
+        drink =  "Latte Affogato"
+        break
+    else:
+        print("choose from the menu\n")
+
+coffee_machine.power_on()
+# coffee_machine.power_off()
+coffee_machine.brew(drink)
