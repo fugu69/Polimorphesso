4974572a64331b98872419e926b0927c7fb57049
Maximus Brutalis <dmitroid1sk@gmail.com>
2025-06-09 12:04:55 +0700

    Impove user input handling with one function instead of while loop and a function

--- cafe.py
+++ cafe.py
@@ -2,27 +2,24 @@ from coffee_machine import CoffeeMachine
 
 coffee_machine = CoffeeMachine()
 
-def user_input():
-    return input("Choose your drink:\n1. Hot Espresso\n2. Cold Espresso\n3. Hot Latte\n4. Latte Affogato\n")
+def get_user_input():
+    menu = {
+        "1": "Hot Espresso",
+        "2": "Cold Espresso",
+        "3": "Hot Latte",
+        "4": "Latte Affogato",
+    }
 
-while True:
-    drink = user_input()
-    
-    if drink =="1":
-        drink = "Hot Espresso"
-        break
-    elif drink == "2":
-        drink = "Cold Espresso"
-        break
-    elif drink == "3":
-        drink = "Hot Latte"
-        break
-    elif drink == "4":
-        drink =  "Latte Affogato"
-        break
-    else:
-        print("choose from the menu\n")
+    while True:
+        choice = input("Choose your drink:\n1. Hot Espresso\n2. Cold Espresso\n3. Hot Latte\n4. Latte Affogato\n")
+        if choice in menu:
+            return menu[choice]
+
+        print("Choose an option from the menu.\n")
+
+
 
 coffee_machine.power_on()
 # coffee_machine.power_off()
+drink = get_user_input()
 coffee_machine.brew(drink)
